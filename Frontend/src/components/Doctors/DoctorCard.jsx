0f186bc96f2a4e2cd6c7277ef7d09151313efd77
Aniket Chavan <anikettchavan103@gmail.com>
2024-03-26 12:34:36 +0530

    s

--- Frontend/src/components/Doctors/DoctorCard.jsx
+++ Frontend/src/components/Doctors/DoctorCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import startIcon from "../../assets/images/star.png";
+import startIcon from "../../assets/images/Star.png";
 import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs";
 
