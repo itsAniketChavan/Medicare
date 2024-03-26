a6cdc17de6428d3342d3a72009b16f2608367944
Aniket Chavan <anikettchavan103@gmail.com>
2024-03-26 13:07:13 +0530

    s

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://medicare-hg0q.onrender.com/";
+export const BASE_URL = "https://medicare-hg0q.onrender.com/api/v1";
 export const token = localStorage.getItem('token')
