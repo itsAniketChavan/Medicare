03ed21d89e0cd5bba25446241e968e84d2627c15
Aniket Chavan <anikettchavan103@gmail.com>
2024-03-26 19:24:57 +0530

    s

--- backend/Controllers/bookingController.js
+++ backend/Controllers/bookingController.js
@@ -14,7 +14,7 @@ export const getCheckoutSession = async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
-      success_url: `${process.env.CLIENT_SITE_URL}/checkout-success`,
+      success_url: `https://medicare-fronted.vercel.app/checkout-success`,
       cancel_url: `${req.protocol}://${req.get('host')}/doctors/${doctor.id}`,
       customer_email:user.email, 
       client_reference_id:req.params.doctorId,
