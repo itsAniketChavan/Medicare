b2acebec555167ea5a475d9d3686655adcfeb3f2
Aniket Chavan <anikettchavan103@gmail.com>
2024-03-26 16:23:01 +0530

    doctorcard borders added

--- Frontend/src/components/Doctors/DoctorCard.jsx
+++ Frontend/src/components/Doctors/DoctorCard.jsx
@@ -14,8 +14,13 @@ const DoctorCard = ({ doctor }) => {
   } = doctor;
   return (
     <div className="p-3 lg:p-5">
-      <div>
-        <img src={photo} className="w-full" alt="" />
+
+      <div style={{ width: "300px", height: "300px",border: "1px solid #ccc" }}>
+        <img
+          src={photo}
+          className="w-full h-full object-cover"
+          alt={name}
+        />
       </div>
 
       <h2
