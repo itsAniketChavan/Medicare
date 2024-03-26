dd1f268062f825104ab277c9fdf42ff3f9665f71
Aniket Chavan <anikettchavan103@gmail.com>
2024-03-26 12:24:31 +0530

    s

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import heroImg03 from "../assets/images/hero-img03.png";
 import icon01 from "../assets/images/icon01.png";
 import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
-import featureImg from "../assets/images/feature-Img.png";
+import featureImg from "../assets/images/feature-img.png";
 import videoIcon from "../assets/images/video-icon.png";
 import avatorImg from "../assets/images/avatar-icon.png";
 import faqImg from "../assets/images/faq-Img.png";
